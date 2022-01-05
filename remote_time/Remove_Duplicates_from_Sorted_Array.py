e590b711159f8aee967c2b95adb8fe97d161e9ba
teke20 <72702993+teke20@users.noreply.github.com>
2022-01-05 09:36:19 -0800

    26. Remove Duplicates from Sorted Array
    leetCode

---
+++ remote_time/Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 1
+        
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[left] = nums[r]
+                left += 1
+        return left
+        
+        

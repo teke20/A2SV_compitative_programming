56f40f701e85a14ea9ed51c1a876b76247341359
teke20 <72702993+teke20@users.noreply.github.com>
2022-01-18 04:17:49 -0800

    Create exception.py

---
+++ week_4/exception.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range (n):
+    try:
+        a,b = input().split()
+        print(int(a)//int((b)))
+    except ZeroDivisionError as e:
+        print("Error Code:", e);
+    except ValueError as v:
+        print("Error Code:", v);

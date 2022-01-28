5d959a0c0130c358f65cd12cf30d62e7defcdd66
teke20 <72702993+teke20@users.noreply.github.com>
2022-01-28 06:39:32 -0800

    Create sort-colors.py

---
+++ week5/sort-colors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            for j in range(1,len(nums)):
+                if nums[j]<nums[j-1]:
+                    nums[j],nums[j-1] = nums[j-1],nums[j] 
+                    
+        
